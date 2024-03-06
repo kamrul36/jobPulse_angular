9a1e365865a45b1b0ac8382ad73e9ec6c168a30b
kamrul36 <mk_hasan2012@yahoo.com>
2024-03-06 08:49:34 +0600

    some components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './public_components/home-page/home-page.component';
 
 const routes: Routes = [
-  {path:'', component: HomePageComponent}
+  {path:'', component: HomePageComponent},
+
+
+  {
+    path: 'admin', loadChildren: () => import('./admin_components/admin/admin.module').then(admin =>  admin.AdminModule)
+  },
+
+  {
+    path: 'employer', loadChildren: () => import('./employer_components/employer/employer.module').then(employer =>  employer.EmployerModule)
+  },
+
+  {
+    path: 'jobseeker', loadChildren: () => import('./job_seeker_components/jobseeker/jobseeker.module').then(jobSeeker =>  jobSeeker.JobseekerModule)
+  },
 ];
 
 @NgModule({
